6893971f54dc84cc7ebb734c2f2ba1c060a0c7aa
J-EUM <eumjungmin1@gmail.com>
2023-01-11 23:36:44 +0900

    변수선언

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main // go 파일은 패키지선언으로 시시작작한한다다. 메
 
 import "fmt" // fmt: format 패키지
 
-func main() { // 모든 메인 패패키키지지는 메메인  함함수수가  있있다다. 키키보보드드왜왜이이래  못못쓰쓰겠겠다다, 메인함수는 매개변수 노노
-	fmt.Println("Hello, world!")
-	fmt.Print("개행안해")
-	fmt.Print("안해")
-	sayHelloWorld("Hello world again!")
+func main() {
+	// var whatToSay string
+	// whatToSay = "Hello world, again!"
+
+	whatToSay := "Hello world, again!" // 알아서 스트링 할당
+
+	sayHelloWorld(whatToSay)
 }
 
 func sayHelloWorld(whatToSay string) {
