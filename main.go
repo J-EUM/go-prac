30f879d8ff38fd3cf23d04059e83186ca1b8890f
J-EUM <eumjungmin1@gmail.com>
2023-01-15 23:52:12 +0900

    패키지 임포트

--- main.go
+++ main.go
@@ -1,16 +1,14 @@
 package main // go 파일은 패키지선언으로 시시작작한한다다. 메메인파트 패패키키지
 
-import "fmt" // fmt: format 패키지
+import (
+	"fmt" // fmt: format 패키지
+	"myapp/doctor"
+)
 
 func main() {
 	// var whatToSay string
-	// whatToSay = "Hello world, again!"
+	// whatToSay = doctor.Intro()
+	whatToSay := doctor.Intro()
 
-	whatToSay := "Hello world, again!" // 알아서 스트링 할당
-
-	sayHelloWorld(whatToSay)
-}
-
-func sayHelloWorld(whatToSay string) {
 	fmt.Println(whatToSay)
 }
