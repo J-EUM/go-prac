1d7fd72ecf2aa76aaab28a8a764e1dcd1d4ad049
J-EUM <eumjungmin1@gmail.com>
2023-01-21 02:47:42 +0900

    변수선언

---
+++ variables/main.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+	// 방법1 - 선언하고 그후 할당 (2단계)
+	var firstNumber int
+	firstNumber = 2
+
+	// 다른방법 - 타입과 이름 선언 그리고 값할당
+	var secondNumber = 5
+
+	// 1단계 변수: 이름 선언, 값할당, Go가 타입을 알아내도록 한다.
+	subtraction := 7
+}
