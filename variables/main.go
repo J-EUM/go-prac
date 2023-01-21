4112d38852a0202a09b23f9a914b601a5636810d
J-EUM <eumjungmin1@gmail.com>
2023-01-21 13:21:32 +0900

    숫자맞추기

--- variables/main.go
+++ variables/main.go
@@ -1,13 +1,47 @@
+// 엔터입력을 받고
+// 변수를 활용해서 응답을 출력
+
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+// 상수는 선언하고 사용하지 않아도 에러 안남
+const prompt = "and press ENTER when ready."
+
 func main() {
-	// 방법1 - 선언하고 그후 할당 (2단계)
-	var firstNumber int
-	firstNumber = 2
-
-	// 다른방법 - 타입과 이름 선언 그리고 값할당
+	var firstNumber = 2
 	var secondNumber = 5
+	var subtraction = 7
+	var answer int // 값을 주지 않으면 기본값 0이 들어간다. string이면 빈스트링
 
-	// 1단계 변수: 이름 선언, 값할당, Go가 타입을 알아내도록 한다.
-	subtraction := 7
+	reader := bufio.NewReader(os.Stdin)
+
+	// 환영/설명
+	fmt.Println("Guess the Number Game")
+	fmt.Println("----------------------")
+	fmt.Println("")
+
+	fmt.Println("Think of a number between 1 and 10", prompt)
+	reader.ReadString('\n')
+
+	// 게임
+	fmt.Println("Multiply your number by", firstNumber, prompt)
+	reader.ReadString('\n')
+
+	fmt.Println("Now multiply the result by", secondNumber, prompt)
+	reader.ReadString('\n')
+
+	fmt.Println("Divide the result by the number you originally thought of", prompt)
+	reader.ReadString('\n')
+
+	fmt.Println("Now subtract", subtraction, prompt)
+	reader.ReadString('\n')
+
+	// 정답
+	answer = firstNumber*secondNumber - subtraction
+	fmt.Println("The answer is", answer)
 }
