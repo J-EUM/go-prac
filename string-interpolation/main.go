67f618a1dda132fb3b0e6aee3c09e33107d1d97d
J-EUM <eumjungmin1@gmail.com>
2023-01-23 01:18:38 +0900

    챌린지

--- string-interpolation/main.go
+++ string-interpolation/main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 	"strconv"
 	"strings"
+
+	"github.com/eiannone/keyboard"
 )
 
 var reader *bufio.Reader
@@ -32,6 +35,7 @@ func main() {
 	user.UserName = readString("What is your name?")
 	user.Age = readInt("How old are you?")
 	user.FavoriteNumber = readFloat("What is your favorite number?")
+	user.OwnsADog = readBool("Do you own a dog (y/n)?")
 
 	// +로 연결 concatination하면 느리고 메모리도 많이쓴다. ,로연결하면 사이사이 스페이스들어간다
 	// fmt.Println("Your name is "+userName+". You are", age, "years old.")
@@ -39,10 +43,11 @@ func main() {
 	// Sprintf: S스트링 프린트 f포맷
 	//fmt.Println(fmt.Sprintf("Your name is %s. You are %d years old.", userName, age))
 	// fmt.Println(fmt.Sprintf()) == fmt.Printf()
-	fmt.Printf("Your name is %s, and you are %d years old. Your favorite number is %.2f.\n",
+	fmt.Printf("Your name is %s, and you are %d years old. Your favorite number is %.2f. Owns a dog: %t.\n",
 		user.UserName,
 		user.Age,
-		user.FavoriteNumber)
+		user.FavoriteNumber,
+		user.OwnsADog)
 }
 
 func prompt() {
@@ -51,7 +56,6 @@ func prompt() {
 
 func readString(s string) string {
 	for {
-
 		fmt.Println(s)
 		prompt()
 
@@ -100,4 +104,35 @@ func readFloat(s string) float64 {
 			return num
 		}
 	}
+}
+
+// 키보드입력 y면 true n면 false리턴
+func readBool(s string) bool {
+	err := keyboard.Open()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer func() {
+		_ = keyboard.Close()
+	}()
+
+	for {
+		fmt.Println(s)
+		prompt()
+
+		char, _, err := keyboard.GetSingleKey()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if char == 'n' || char == 'N' {
+			return false
+		} else if char == 'y' || char == 'Y' {
+			return true
+		} else {
+			fmt.Println("y/n만 입력하셈")
+		}
+	}
+
 }
