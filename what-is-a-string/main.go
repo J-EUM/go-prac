0347137541f15cad3bd630e8b37f4af10c9de9d9
J-EUM <eumjungmin1@gmail.com>
2023-02-17 10:36:31 +0900

    what is a string

---
+++ what-is-a-string/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	fmt.Println()
+	name := "Hello world"
+	fmt.Println("String: ", name)
+	fmt.Println()
+
+	// string: a slice of bytes
+	fmt.Println("Bytes")
+	for i := 0; i < len(name); i++ {
+		fmt.Printf("%x ", name[i])
+	}
+
+	fmt.Println()
+	fmt.Println()
+
+	// string: a slice of runes.
+	// rune: 32bit integer 룬 유니코드값 저장가능
+	fmt.Println("Index\tRune\tSting")
+	for x, y := range name {
+		fmt.Println(x, "\t", y, "\t", string(y))
+	}
+	fmt.Println()
+
+	name = "안녕하세요요"
+	fmt.Println("Index\tRune\tSting")
+	for x, y := range name {
+		fmt.Println(x, "\t", y, "\t", string(y))
+	}
+
+	fmt.Println()
+	fmt.Println()
+
+	fmt.Println("문자열붙이기 3방법")
+
+	h := "Hello, "
+	w := "world."
+
+	// 스트링은 immutable -> 별로 좋은방법아니다
+	myString := h + w
+	fmt.Println(myString)
+
+	// using fmt - +보다 효율적
+	myString = fmt.Sprintf("%s%s", h, w)
+	fmt.Println(myString)
+
+	// using stringbuilder - 매우 efficient, more control
+	var sb strings.Builder
+	sb.WriteString(h)
+	sb.WriteString(w)
+	fmt.Println(sb.String())
+
+	fmt.Println()
+
+	// substring
+	name = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	fmt.Println("Getting a substring")
+	fmt.Println(name[10:13])
+}
