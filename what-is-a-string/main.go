e3e419045e1987f84e7fe78657f31d4b4f618772
J-EUM <eumjungmin1@gmail.com>
2023-02-17 13:08:09 +0900

    the strings package

--- what-is-a-string/main.go
+++ what-is-a-string/main.go
@@ -6,73 +6,32 @@ import (
 )
 
 func main() {
-	fmt.Println()
-	name := "Hello world"
-	fmt.Println("String: ", name)
-	fmt.Println()
-
-	// string: a slice of bytes
-	fmt.Println("Bytes")
-	for i := 0; i < len(name); i++ {
-		fmt.Printf("%x ", name[i])
+	courses := []string{
+		"go Go 고고",
+		"java",
+		"python",
 	}
 
-	fmt.Println()
-	fmt.Println()
-
-	// string: a slice of runes.
-	// rune: 32bit integer 룬 유니코드값 저장가능
-	fmt.Println("Index\tRune\tSting")
-	for x, y := range name {
-		fmt.Println(x, "\t", y, "\t", string(y))
-	}
-	fmt.Println()
-
-	name = "안녕하세요요"
-	fmt.Println("Index\tRune\tSting")
-	for x, y := range name {
-		fmt.Println(x, "\t", y, "\t", string(y))
+	for _, x := range courses {
+		// x 가 "Go" 를 포함하고 있으면 true
+		if strings.Contains(x, "Go") {
+			fmt.Println("Go is found in", x, "index is", strings.Index(x, "Go"))
+		}
 	}
 
-	fmt.Println()
-	fmt.Println()
+	newString := "Go is a great programming language. Go for it!"
 
-	fmt.Println("문자열붙이기 3방법")
-
-	h := "Hello, "
-	w := "world."
-
-	// 스트링은 immutable -> 별로 좋은방법아니다
-	myString := h + w
-	fmt.Println(myString)
-
-	// using fmt - +보다 효율적
-	myString = fmt.Sprintf("%s%s", h, w)
-	fmt.Println(myString)
-
-	// using stringbuilder - 매우 efficient, more control
-	var sb strings.Builder
-	sb.WriteString(h)
-	sb.WriteString(w)
-	fmt.Println(sb.String())
-
-	fmt.Println()
-
-	// substring
-	name = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	fmt.Println("Getting a substring")
-	fmt.Println(name[10:13])
-
-	fmt.Println()
-	fmt.Println()
-
-	// 인덱스
-	//             01234567890123456789012345678901234
-	courseName := "Learn Go for Beginners Crash Course"
-
-	for i := 13; i <= 21; i++ {
-		// string() 캐스트를 하지 않으면 룬(int32)형태로 출력
-		fmt.Print(string(courseName[i]))
-	}
+	// newString이 "Go"로 시작하면 true
+	fmt.Println(strings.HasPrefix(newString, "Go"))
+	// newString이 "Go"로 끝나면 true
+	fmt.Println(strings.HasSuffix(newString, "Go")) // false
+	fmt.Println(strings.HasSuffix(newString, "!"))  // true
+	// newString에 "Go"가 몇개 있는지 숫자 반환
+	fmt.Println(strings.Count(newString, "Go")) // 2
+	// newString에서 첫 "Go"의 위치(인덱스), 없다면 -1
+	fmt.Println(strings.Index(newString, "Go")) //0
+	fmt.Println(strings.Index(newString, "oG")) // -1
+	// newString에서 마지막 "Go"의 인덱스
+	fmt.Println(strings.LastIndex(newString, "Go")) // 36
 
 }
